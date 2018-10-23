b68f396b1337b6fa13197a9ea54c185b8d429243
Chris Goldsworthy <christopher.goldsworthy@outlook.com>
2018-10-23 12:40:23 -0400

    Storing what I did for demontrating CI stuff

--- ci/test-jailhouse-hardware-check.sh
+++ ci/test-jailhouse-hardware-check.sh
@@ -3,7 +3,13 @@
 PASS_DIR=~/programming/jailhouse/dev_env/passthrough_dir
 DISK=~/programming/jailhouse/dev_env/dev_disk.qcow2
 
-run_intel() {
+
+# ================================
+# BATS specific helpers
+# ================================
+
+# This is a function called by BATS at the end of every @test.
+setup() {
 	sudo /home/oem/Desktop/qemu/build/x86_64-softmmu/qemu-system-x86_64 \
 		-machine q35,kernel_irqchip=split -m 1G -enable-kvm \
 		-smp 4 -device intel-iommu,intremap=on,x-buggy-eim=on \
@@ -15,52 +21,20 @@ run_intel() {
 		-device virtio-9p-pci,fsdev=pass_thr,addr=1f.7,mount_tag=passthrough_dir \
 		-daemonize -pidfile pid
 
-	ssh root@localhost -T -p 5555 ls &> /dev/null
-	RET=$?
-
-	while [ "$RET" != "0" ]; do
-		ssh root@localhost -T -p 5555 ls &> /dev/null
-		RET=$?
-		sleep 5
-	done
-
 	ssh root@localhost -T -p 5555 <<-HEREDOC
 	mount -t 9p passthrough_dir dev
 	HEREDOC
 }
 
-run_amd() {
-	/home/oem/Desktop/qemu/build/x86_64-softmmu/qemu-system-x86_64 \
-		-machine q35,kernel_irqchip=split -m 1G \
-		-smp 4 \
-		-cpu Opteron_G5,+svm,+npt \
-		-hda ${DISK} \
-		-netdev user,id=net,hostfwd=tcp::5555-:22 -device e1000e,addr=2.0,netdev=net \
-		-fsdev local,id=pass_thr,path=${PASS_DIR},security_model=passthrough \
-		-device virtio-9p-pci,fsdev=pass_thr,addr=1f.7,mount_tag=passthrough_dir \
-		-device amd-iommu \
-		-daemonize -pidfile pid
-
-	ssh root@localhost -T -p 5555 ls &> /dev/null
-	RET=$?
-
-	while [ "$RET" != "0" ]; do
-		ssh root@localhost -T -p 5555 ls &> /dev/null
-		RET=$?
-		sleep 5
-	done
-
-	ssh root@localhost -T -p 5555 <<-HEREDOC
-	mount -t 9p passthrough_dir dev
-	HEREDOC
-}
-
+# This is a function called by BATS at the end of every @test.
 teardown() {
 	# Shutdown, prepare for next instance
 	ssh root@localhost -T -p 5555 <<-HEREDOC
 	shutdown now
 	HEREDOC
 
+	# Wait for QEMU to shutdown before proceeding, to prevent an early startup
+	# of QEMU occuring once the next test begins (which may fail)
 	while [ "$(ps aux | grep $(sudo cat pid) | grep -v grep)" != "" ]; do
 		sleep 5
 	done
@@ -68,35 +42,40 @@ teardown() {
 	sudo rm -f pid
 }
 
-test_tmp_rm_tmp() {
+# ================================
+# BATS-independent helpers
+# ================================
+
+run_python_script_with_coverage() {
+	# Run the command in the QEMU instance and save the return value in tmp
+	# (on the instance).  Note that the python script (stored in $1) is being 
+	# executed through the python coverage program `coverage`
+	ssh root@localhost -T -p 5555 <<-HEREDOC
+	coverage run -p --include="*jailhouse*" $1
+	echo $? > tmp
+	HEREDOC
+
+	# Get the result stored in tmp via scp, remove tmp on the instance
 	scp -P 5555 root@localhost:tmp .
 	ssh root@localhost -T -p 5555 "rm -f tmp"
+
+	# Load value of tmp (on the host) into variable, remove tmp on the host,
+	# return result	
 	RES=$(cat tmp)
 	rm -f tmp
 	[ "$RES" == 0 ]
 }
 
-test_hw_check() {
-	ssh root@localhost -T -p 5555 <<-HEREDOC
-	coverage run -p --include="*jailhouse*" jailhouse/tools/jailhouse-hardware-check
-	echo $? > tmp
-	HEREDOC
+# ================================
+# TESTS
+# ================================
 
-	test_tmp_rm_tmp
-}
+# BATS executes each of these in the order they're instantiated in.
 
-test_config_create() {
-	ssh root@localhost -T -p 5555 <<-HEREDOC
-	coverage run -p --include="*jailhouse*" jailhouse/tools/jailhouse-config-create sys.c
-	echo $? > tmp
-	HEREDOC
-
-	test_tmp_rm_tmp
-}
-
+# Although BATS has a setup() and teardown() function for each individual test,
+# it lacks such setup / teardown functions that run before / after all tests.
+# Thus, this "test" is used as a setup function.
 @test "pre-testing setup" {
-
-	run_intel
 
 	# Some setup stuff 
 	ssh root@localhost -T -p 5555 <<-HEREDOC
@@ -107,33 +86,24 @@ test_config_create() {
 	HEREDOC
 }
 
+# The only legitimate test here - both instances of [ "$?" == "0" ] must
+# evaluate to 0 for the test to succeed (the test fails whenever such an 
+# expression evaluates to a non-zero value)
 @test "intel" {
 
-	run_intel
-
-	test_hw_check
+	run_python_script_with_coverage jailhouse/tools/jailhouse-hardware-check
 	[ "$?" == "0" ]
-	test_config_create
+	run_python_script_with_coverage jailhouse/tools/jailhouse-config-create sys.c
 	[ "$?" == "0" ]
 }
 
-@test "amd" {
-	skip
-
-	run_amd
-
-	test_hw_check
-	[ "$?" == "0" ]
-	test_config_create
-	[ "$?" == "0" ]
-}
-
+# Counterpart to @test "pre-testing setup"
 @test "post-testing teardown" {
 
-	run_intel
-
-	# Pull coverage information, generate docs, shutdown machine
+	# Pull coverage information
 	scp -P 5555 root@localhost:.coverage.* .
+
+	# Generate pretty coverage docs
 	coverage combine .coverage.*
 	sed -i "s/\/root\/dev/\/home\/oem\/programming\/jailhouse\/dev_env\/passthrough_dir/g" .coverage
 	coverage html -i
